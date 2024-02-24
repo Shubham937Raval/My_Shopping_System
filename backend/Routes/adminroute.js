a5abdcc39ba3645d2e785fe4e13f096d4b24aaab
Shubham Raval <79586633+Shubham937Raval@users.noreply.github.com>
2024-02-24 15:39:49 +0530

    Add files via upload

--- backend/Routes/adminroute.js
+++ backend/Routes/adminroute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {superAdminRegister,adminLogin,tokenDestroy,profileData,getdashboardData,postdashboardData,orderData,productData,custmorData,reportData,integrationData,Setting} = require('../Controller/AdminController')
+const {CategoryListData,deleteProductCategory,admindetailsAPI,ProductListData,superAdminRegister,adminLogin,tokenDestroy,profileData,getdashboardData,orderData,productData,postproductData,custmorData,reportData,integrationData,Setting} = require('../Controller/AdminController')
 const router = express.Router();
 
 router.get('/',(req,res)=>{res.render('login')});
@@ -14,13 +14,15 @@ router.get('/create-admin',(req,res)=>{res.render('registration')})
 router.post('/create-admin',superAdminRegister)
 
 router.get('/dashboard',getdashboardData)
-router.post('/dashboard',postdashboardData)
+router.post('/dashboard',async(req,res)=>{})
 
 router.get('/order',orderData)
 router.post('/order',(req,res)=>{})
 
 router.get('/product',productData)
-router.post('/product',(req,res)=>{})
+router.post('/product',postproductData)
+
+router.get('/deleteProductcategory/:name',deleteProductCategory)
 
 router.get('/customer',custmorData)
 router.post('/customer',(req,res)=>{})
@@ -37,4 +39,7 @@ router.post('/product',(req,res)=>{})
 router.get('/profile',profileData)
 router.post('/profile',(req,res)=>{})
 
+router.get('/admindetails',admindetailsAPI)
+router.get('/shoppinglist/:categoryname',ProductListData);
+router.get('/shoppinglist',CategoryListData);
 module.exports = router;
